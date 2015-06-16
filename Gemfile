3552cfe2f13dd87d5438e005931cdd294703bd80
quyetdc <quyetdc@evolableasia.vn>
2015-06-16 16:08:17 +0700

    create message and conversations

--- Gemfile
+++ Gemfile
@@ -47,3 +47,6 @@ gem 'devise'
 
 gem "therubyracer"
 gem "twitter-bootstrap-rails"
+
+gem 'private_pub'
+gem 'thin'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require chat
+//= require private_pub
 //= require turbolinks
 //= require_tree .
