bc10b34e0d13836a54d75a9f33ee5ccfe3e4a57a
quyetdc <quyetdc@evolableasia.vn>
2015-06-19 15:58:25 +0700

    auto popup chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,12 @@ class MessagesController < ApplicationController
     @message.user_id = current_user.id
     @message.save!
 
+    # This path is used in create.js.erb to publish notification to this channel
     @path = conversation_path(@conversation)
 
+    # Publish notification to recipient channel
+    rip = current_user == @conversation.recipient ? @conversation.sender : @conversation.recipient
+    PrivatePub.publish_to("/notifications" + rip.id.to_s, cid: @conversation.id, sid: current_user.id, rip: rip.id)
     # render json: { message: @message }
   end
 
